b87213c6af8178bb7e4dd815bf4e5443935ac4c0
hamzaalbaghdady <hamzaalbaghdady971@gmail.com>
2025-04-12 17:36:34 +0300

    Done with Authentication.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Authentication routes:
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
 });
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout'])->name('logout');
+
